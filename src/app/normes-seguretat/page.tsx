8f96649dcd98760b9936266ad268ccdac52c1a94
IT Menadiona <menadionait@gmail.com>
2025-10-01 14:22:34 +0000

    alinealo a la derecha

--- src/app/normes-seguretat/page.tsx
+++ src/app/normes-seguretat/page.tsx
@@ -63,7 +63,7 @@ function SafetyNormsContent() {
             <p className="text-[10px] text-muted-foreground font-body">
               En compliment del Reglament (UE) 2016/679 (RGPD) i la Llei 3/2018 (LOPDGDD), l'informem que les seves dades seran tractades per 'Menadiona, SL' amb la finalitat de gestió i control de la informació de seguretat de les seves instal·lacions. Pot exercir els seus drets dirigint-se a 'Menadiona, SL' per e-mail a menadiona@menadiona.com. Més informació en www.menadiona.com
             </p>
-             <p className="mt-2 text-[10px] text-muted-foreground font-body">
+             <p className="mt-2 text-[10px] text-muted-foreground font-body text-right">
               SHE N.S.T.004/05.24
             </p>
           </div>
