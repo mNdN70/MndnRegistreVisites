cabaa4f7944e3c943448efa72236d5e373a982d0
IT Menadiona <menadionait@gmail.com>
2025-10-01 10:24:10 +0000

    Cambia la fuente de la aplicacióna a Arial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Belleza', 'sans-serif'],
+        body: ['Arial', 'sans-serif'],
+        headline: ['Arial', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
