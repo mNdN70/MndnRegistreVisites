42e3eb117f0c9b6e28e5690a60d5c31e35561600
IT Menadiona <menadionait@gmail.com>
2025-11-28 15:23:18 +0000

    como puedo publicar el proyecto en netlify?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6638906933-56dae",
-  "appId": "1:1010390430341:web:4b9f5326353e6c2da22fec",
-  "apiKey": "AIzaSyCSX3ybx1NUc3ph2gPuZumnhhoXdlM_p_U",
-  "authDomain": "studio-6638906933-56dae.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1010390430341"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-6638906933-56dae",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1010390430341:web:4b9f5326353e6c2da22fec",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCSX3ybx1NUc3ph2gPuZumnhhoXdlM_p_U",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-6638906933-56dae.firebaseapp.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1010390430341",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-1WJ2219MV3",
 };
